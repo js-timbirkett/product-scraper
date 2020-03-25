8f183fae538eb8efde62fa0a2fbbc55af7ba54c6
Tim Birkett <tim.birkett@sainsburys.co.uk>
2020-03-25 15:56:21 +0000

    chore: Initial commit

---
+++ src/main/java/co/uk/sainsburys/scraper/cli/App.java
@@ -0,0 +1,13 @@
+package co.uk.sainsburys.scraper.cli;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
